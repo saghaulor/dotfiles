5dd5b7ee0d481c539b4263435ec324781ee3c07f
Stephen Aghaulor <saghaulor@gmail.com>
2013-09-28 16:26:39 -0700

    Added method for detecting if homebrew exists and installing it if not.

--- ruby/brew.rb
+++ ruby/brew.rb
@@ -1,3 +1,23 @@
+def which(cmd)
+  exts = ENV['PATHEXT'] ? ENV['PATHEXT'].split(';') : ['']
+  ENV['PATH'].split(File::PATH_SEPARATOR).each do |path|
+    exts.each { |ext|
+      exe = File.join(path, "#{cmd}#{ext}")
+      return exe if File.executable? exe
+    }
+  end
+  return nil
+end
+
+unless which 'brew'
+  homebrew = File.join('/tmp', 'homebrew.rb')
+  File.open(homebrew, 'a+') do |file|
+    file.write("#{`curl -fsSL https://raw.github.com/mxcl/homebrew/go`}")
+  end
+
+  load homebrew
+end
+
 kegs = %w(vim tmux rbenv ruby-build rbenv-gem-rehash rbenv-default-gems git 
   ctags bash-completion zsh node zsh-syntax-highlighting zsh-completions)
 cmd = 'brew install '
